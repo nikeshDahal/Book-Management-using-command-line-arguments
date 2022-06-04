909ae26b0da98dc07a7de23b4f56e3c1e349ddf6
nikeshDahal <nikeshdahal297@gmail.com>
2022-06-04 11:06:28 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+console.log("welcome to Book Management");
+console.log("select your options ....");
+console.log(
+  " 1 .create books\n2. get book by name\n3. update book by name\n4. get all book list\n5. search book\n6. delete book by name\n"
+);
+
+switch (inputValue) {
+  case 1:
+    console.log("create option selected..");
+    break;
+  case 2:
+    console.log("get option selected..");
+    break;
+  case 3:
+    console.log("update option selected..");
+    break;
+  case 4:
+    console.log("get all option selected..");
+    break;
+  case 5:
+    console.log("search all option selected..");
+    break;
+  case 6:
+    console.log("delete all option selected..");
+    break;
+}
