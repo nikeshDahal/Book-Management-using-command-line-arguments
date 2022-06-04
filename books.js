61045d30d23857f1bf9c4f2e323bff940b131607
sumaly01 <sumalybajra@gmail.com>
2022-06-04 22:44:15 +0545

    CRUD operations function added

--- books.js
+++ books.js
@@ -1,8 +1,10 @@
 const fs = require('fs')
+// const { get } = require('http')
 const process = require('process')
+const bookController = {}
 
 
-const addBook = (name,author,genre) => {
+bookController.addBook = (name,author,genre) => {
     fs.access('./books', error => {
         if(error){
             fs.mkdir("./books", err => {
@@ -14,41 +16,140 @@ const addBook = (name,author,genre) => {
             })
         }
     
-    process.chdir('./books')
+    // process.chdir('./books')
     const books = loadBooks(genre)
-        const duplicateBook = books.find((book) => book.name === name)
+    if(books.length == 0) {
+        console.log('Adding new genre')
+    }
+    const duplicateBook = books.find((book) => book.name == name)
+
+    if (!duplicateBook) {
+        books.push({
+            name: name,
+            author: author,
+            genre:genre
+        })
+        savebooks(books,genre)
+        console.log('New book added!')
+    } else {
+        console.log('Book name taken!')
+    }
+})
+}
+
+// const getBookByName = (name,genre) => {
+//     // process.chdir('./books')
+//     const books = loadBooks(genre)
+//     const book = books.find((book) => book.name === name)
     
-        if (!duplicateBook) {
-            books.push({
-                name: name,
-                author: author,
-                genre:genre
-           })
-            savebooks(books,genre)
-            console.log('New book added!')
-        } else {
-            console.log('Book name taken!')
+//     if (book) {
+//         console.log(`Book`,book)
+//     } else {
+//         console.log('book not found!')
+//     }
+// }
+
+bookController.getBookByName = (name) => {
+    
+    const allBooks = loadAllBooks()
+    console.log('Getting the matched book name from all genres')
+    let bookFound = false
+    allBooks.forEach(books => {
+        const book = books.find((book) => book.name === name)
+        if(book){
+        console.log(book)
+        bookFound = true
         }
     })
+    if (!bookFound){
+        console.log('No matching book name')
+    }
+  
+}
+
+bookController.getAllBook = () => {
+    
+    // const arrayOfFiles = fs.readdirSync("./books")
+    // let books
+    // arrayOfFiles.forEach(fileName => {
+    //     fileName =fileName.split('.')[0]
+    //     books = loadBooks(fileName)
+    //     console.log(books)
+
+    // });
+    console.log('All books', loadAllBooks());
+}
+
+bookController.updateBookByName = (name,genre,newName) => {
+    // process.chdir('./books')
+    const books = loadBooks(genre)
+    const updatedBooks = []
+    let bookFound = false
+    books.forEach(tempBook => {
+        if(tempBook.name === name) {
+            tempBook.name = newName
+            bookFound = true
+        }
+          updatedBooks.push(tempBook)
+    })
+    if(bookFound) {
+        savebooks(updatedBooks,genre)
+        // process.chdir('..')
+        getAllBook()
+    } else {
+        console.log("Book not found")
+    }
+ 
+}
+
+bookController.deleteBookByName = (name,genre) => {
+    const books = loadBooks(genre)
+    const tempBooks = []
+    let bookFound = false
+    books.forEach(tempBook => {
+        if(tempBook.name !== name) {
+           tempBooks.push(tempBook)
+        } else {
+            bookFound = true
+        }
+    })
+    if(bookFound) {
+        savebooks(tempBooks,genre)
+        console.log(`Removed book = ${name} and updated book list is `)
+        getAllBook()
+    } else {
+        console.log("Book not found")
+    }
+ 
 }
 
 const savebooks = (books,genre) => {
     const dataJSON = JSON.stringify(books)
-    fs.writeFileSync(`${genre}.json`, dataJSON)
+    fs.writeFileSync(`./books/${genre}.json`, dataJSON)
 }
 
 const loadBooks = (genre) => {
     try {
-        const dataBuffer = fs.readFileSync(`${genre}.json`)
+        const dataBuffer = fs.readFileSync(`./books/${genre}.json`)
         const dataJSON = dataBuffer.toString()
         return JSON.parse(dataJSON)
-    } catch (e) {
+    } catch (err) {
+        console.log('Genre not found',err)
         return []
     }
+}
+
+const loadAllBooks = () => {
+    const arrayOfFiles = fs.readdirSync("./books")
+    let booksArray = []
+    arrayOfFiles.forEach(fileName => {
+        fileName = fileName.split('.')[0]
+        booksArray.push(loadBooks(fileName))
+    });
+    return booksArray
+    // console.log('booksArray',booksArray)
 
 }
 
 
-module.exports = {
-    addBook:addBook
-}
+module.exports = bookController
